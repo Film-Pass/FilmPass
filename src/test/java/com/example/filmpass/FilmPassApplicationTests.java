b1d80eebce9e36bd1114a1d50efabd0f36bba731
Hyeon woo <renzpark@naver.com>
2025-07-20 18:30:47 +0900

    okey

---
+++ src/test/java/com/example/filmpass/FilmPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

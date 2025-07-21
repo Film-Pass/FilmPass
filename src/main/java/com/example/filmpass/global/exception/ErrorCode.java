e43e3c5d951ac6d3a8a3261b4b73546273603dad
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-21 17:12:26 +0900

    feat : 회원가입 예외추가

--- src/main/java/com/example/filmpass/global/exception/ErrorCode.java
+++ src/main/java/com/example/filmpass/global/exception/ErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력 값이 유효하지 않습니다.");
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력 값이 유효하지 않습니다."),
+
+    // Auth
+    EMAIL_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 사용중인 이메일입니다."),
+    NICKNAME_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 사용중인 닉네임입니다.");
 
     private final HttpStatus code;
     private final String message;

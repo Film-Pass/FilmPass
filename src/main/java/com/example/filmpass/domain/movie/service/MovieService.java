f0e6cf5d87a6ff54dcb31a5e2a3a042ca8ee19f5
YoungJae1118 <apricity1118@gmail.com>
2025-07-21 19:51:38 +0900

    feat(#31): 영화 전체 조회 기능 추가

--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -1,9 +1,21 @@
 package com.example.filmpass.domain.movie.service;
 
+import com.example.filmpass.domain.movie.entity.Movie;
+import com.example.filmpass.domain.movie.repository.MoviceRepository;
+import com.example.filmpass.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
+    private final MoviceRepository moviceRepository;
+
+    //영화 목록 전체 조회
+    public ApiResponse<List<Movie>> findAllMovie() {
+        List<Movie> movieList = moviceRepository.findAll();
+        return ApiResponse.success(movieList, "영화목록 조회 성공!");
+    }
 }

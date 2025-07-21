663e86b35af947cc55627e3da050479e0e2f4529
YoungJae1118 <apricity1118@gmail.com>
2025-07-21 17:04:21 +0900

    feat(#30): 영화 등록 기능 추가 CreateMovie

--- src/main/java/com/example/filmpass/domain/movie/controller/MovieController.java
+++ src/main/java/com/example/filmpass/domain/movie/controller/MovieController.java
@@ -1,9 +1,24 @@
 package com.example.filmpass.domain.movie.controller;
 
+import com.example.filmpass.domain.movie.dto.MovieCreateRequest;
+import com.example.filmpass.domain.movie.service.MovieService;
+import com.example.filmpass.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/movies")
 @RequiredArgsConstructor
 public class MovieController {
+    private final MovieService movieService;
+
+    //영화 등록 CreateMovie
+    @PostMapping
+    public ApiResponse<String> movieCreateApi(@RequestBody MovieCreateRequest movieCreateRequest) {
+        return movieService.movieCreate(movieCreateRequest);
+    }
+
 }

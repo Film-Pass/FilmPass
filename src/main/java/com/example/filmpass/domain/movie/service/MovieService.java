8ac822e0bc866f9f691a59e78136d265ab1a3abd
YoungJae1118 <apricity1118@gmail.com>
2025-07-22 16:43:04 +0900

    feat(#31): 영화 전체 조회 기능 추가 feat(#31): 영화 목록이 비어있을 때의 응답 추가

--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -16,6 +16,9 @@ public class MovieService {
     //영화 목록 전체 조회
     public ApiResponse<List<Movie>> findAllMovie() {
         List<Movie> movieList = moviceRepository.findAll();
+        if(movieList.isEmpty()) {
+            return ApiResponse.success(movieList,"영화가 존재하지 않습니다");
+        }
         return ApiResponse.success(movieList, "영화목록 조회 성공!");
     }
 }

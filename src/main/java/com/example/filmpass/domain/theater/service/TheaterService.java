052987bc3e4cb2c6e8a5f3aca89099957fc4e967
권용준 <yoyo91828h@gmail.com>
2025-07-22 20:24:08 +0900

    feat: TheaterService 극장 단건조회 서비스 추가

--- src/main/java/com/example/filmpass/domain/theater/service/TheaterService.java
+++ src/main/java/com/example/filmpass/domain/theater/service/TheaterService.java
@@ -1,9 +1,29 @@
 package com.example.filmpass.domain.theater.service;
 
+import com.example.filmpass.domain.theater.dto.TheaterResponse;
+import com.example.filmpass.domain.theater.entity.Theater;
+import com.example.filmpass.domain.theater.repository.TheaterRepository;
+import com.example.filmpass.global.exception.CustomException;
+import com.example.filmpass.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TheaterService {
+
+    private final TheaterRepository theaterRepository;
+
+
+    // 극장 단건 조회
+    public TheaterResponse getTheaterById(Long id) {
+        Theater theater = theaterRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.THEATER_NOT_FOUND));
+
+        return new TheaterResponse(
+                theater.getId(),
+                theater.getName(),
+                theater.getLocation()
+        );
+    }
 }

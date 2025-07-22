54b585ce50913d313e14ae533a1eecda512eb1c6
YoungJae1118 <apricity1118@gmail.com>
2025-07-22 19:20:48 +0900

    feat(#31): 영화 전체 조회 기능 추가 feat(#31): 영화 목록이 비어있을 때의 응답 추가

--- src/main/java/com/example/filmpass/domain/movie/repository/MoviceRepository.java
+++ src/main/java/com/example/filmpass/domain/movie/repository/MoviceRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MoviceRepository extends JpaRepository<Movie, Long> {
+    Optional<Movie> findByTitle(String title);
+    Optional<Movie> findByDirector(String director);
+
+    List<Movie> id(Long id);
 }
--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -1,11 +1,14 @@
 package com.example.filmpass.domain.movie.service;
 
-import com.example.filmpass.domain.movie.dto.MovieCreateRequest;
+import com.example.filmpass.domain.movie.dto.FindMovieRequest;
 import com.example.filmpass.domain.movie.entity.Movie;
 import com.example.filmpass.domain.movie.repository.MoviceRepository;
 import com.example.filmpass.global.common.ApiResponse;
+import com.example.filmpass.domain.movie.dto.MovieCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 import java.time.LocalDateTime;
 
@@ -26,5 +29,28 @@ public class MovieService {
         moviceRepository.save(movie);
 
         return ApiResponse.success(movie.getTitle(),"영화 등록 성공!");
+    }
+
+    //영화 검색
+    public ApiResponse<Optional<Movie>> findMovie(FindMovieRequest findMovieRequest) {
+        Long id = findMovieRequest.getId();
+        String title = findMovieRequest.getTitle();
+        String director = findMovieRequest.getDirector();
+
+        if(id !=null) {
+            Optional<Movie> movie = moviceRepository.findById(id);
+            return ApiResponse.success(movie,"영화 검색 성공");
+        }
+
+        if(title != null){
+            Optional<Movie> movie = moviceRepository.findByTitle(title);
+            return ApiResponse.success(movie,"영화 검색 성공");
+        }
+
+        if(director != null){
+            Optional<Movie> movie = moviceRepository.findByDirector(director);
+            return ApiResponse.success(movie,"영화 검색 성공");
+        }
+        return ApiResponse.error("입력값에 해당하는 영화가 존재하지 않습니다");
     }
 }

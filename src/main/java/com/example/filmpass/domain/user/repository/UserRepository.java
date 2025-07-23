212e0c5fe155d0f0ac93f5b745126091243e0595
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-23 17:45:15 +0900

    feat : findByEmail 추가

--- src/main/java/com/example/filmpass/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/filmpass/domain/user/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.example.filmpass.domain.user.repository;
 import com.example.filmpass.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByNickname(String nickname);
+
+    Optional<User> findByEmail(String email);
 }

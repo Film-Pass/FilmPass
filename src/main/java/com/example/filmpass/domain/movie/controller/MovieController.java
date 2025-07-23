6a21fe9addfe446a83af0fe4168d8f1e2499bc3d
YoungJae1118 <apricity1118@gmail.com>
2025-07-23 19:38:14 +0900

    feat: 영화 상세 조회 기능 개발

--- src/main/java/com/example/filmpass/domain/movie/controller/MovieController.java
+++ src/main/java/com/example/filmpass/domain/movie/controller/MovieController.java
@@ -1,9 +1,22 @@
 package com.example.filmpass.domain.movie.controller;
 
+import com.example.filmpass.domain.movie.service.MovieService;
+import com.example.filmpass.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/movies")
 @RequiredArgsConstructor
 public class MovieController {
+    private final MovieService movieService;
+
+    //영화 상세 조회
+    @GetMapping("/{movieId}")
+    public ApiResponse<Object> findMovieDetail(@PathVariable Long movieId) {
+        return movieService.findMovieDtail(movieId);
+    }
 }
--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -3,6 +3,7 @@ package com.example.filmpass.domain.movie.service;
 import com.example.filmpass.domain.movie.dto.FindMovieDetailResponse;
 import com.example.filmpass.domain.movie.entity.Movie;
 import com.example.filmpass.domain.movie.repository.MoviceRepository;
+import com.example.filmpass.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,16 @@ import java.util.Optional;
 public class MovieService {
     private final MoviceRepository moviceRepository;
 
-    public void findMovieDtail(Long movieId) {
-        Movie movie = moviceRepository.findById(movieId)
-        FindMovieDetailResponse findMovieDetailResponse = new FindMovieDetailResponse()
+    //영화 상세 조회
+    public ApiResponse<Object> findMovieDtail(Long movieId) {
+        Optional<Movie> optionalMovie = moviceRepository.findById(movieId);
+        if(optionalMovie.isEmpty()) {
+            return ApiResponse.error("Id에 해당하는 영화를 찾을 수 없습니다.");
+        }
+
+        Movie movie = optionalMovie.get();
+        FindMovieDetailResponse findMovieDetailResponse = new FindMovieDetailResponse(movie.getTitle(), movie.getDirector(), movie.getDescription());
+
+        return ApiResponse.success(findMovieDetailResponse, "영화 상세 조회 성공");
     }
 }

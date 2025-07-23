6c245c82c3a18e2405c3289bc82d0fa22f42e2a2
권용준 <yoyo91828h@gmail.com>
2025-07-23 18:11:13 +0900

    feat: TheaterService 극장 수정 기능부분 추가

--- src/main/java/com/example/filmpass/domain/theater/service/TheaterService.java
+++ src/main/java/com/example/filmpass/domain/theater/service/TheaterService.java
@@ -1,9 +1,34 @@
 package com.example.filmpass.domain.theater.service;
 
+
+import com.example.filmpass.domain.theater.dto.TheaterRequest;
+import com.example.filmpass.domain.theater.dto.TheaterResponse;
+import com.example.filmpass.domain.theater.entity.Theater;
+import com.example.filmpass.domain.theater.repository.TheaterRepository;
+import com.example.filmpass.global.exception.CustomException;
+import com.example.filmpass.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class TheaterService {
+
+    private final TheaterRepository theaterRepository;
+
+    // 극장 수정
+    public TheaterResponse updateTheater(Long id, TheaterRequest request) {
+        Theater theater = theaterRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.THEATER_NOT_FOUND));
+
+        theater.update(request.getName(), request.getLocation());
+
+        return new TheaterResponse(
+                theater.getId(),
+                theater.getName(),
+                theater.getLocation()
+        );
+    }
 }

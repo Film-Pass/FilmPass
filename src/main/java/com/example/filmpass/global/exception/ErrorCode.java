9066066089683ee77f32337ff224717a057cf457
권용준 <yoyo91828h@gmail.com>
2025-07-23 18:09:02 +0900

    fix: ErrorCode 극장 수정 추가로 텍스트만 수정

--- src/main/java/com/example/filmpass/global/exception/ErrorCode.java
+++ src/main/java/com/example/filmpass/global/exception/ErrorCode.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    // 극장 수정으로 이름 수정
+    THEATER_NOT_FOUND(HttpStatus.NOT_FOUND, "극장을 찾을 수 없습니다."),
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력 값이 유효하지 않습니다.");
 
     private final HttpStatus code;

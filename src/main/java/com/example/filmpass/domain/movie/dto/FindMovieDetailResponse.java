f9ca4e2f4227c8d13aba1107ed48fe3b67a96fc9
YoungJae1118 <apricity1118@gmail.com>
2025-07-23 19:22:31 +0900

    feat: 영화 상세 조회 기능 개발중

---
+++ src/main/java/com/example/filmpass/domain/movie/dto/FindMovieDetailResponse.java
@@ -0,0 +1,15 @@
+package com.example.filmpass.domain.movie.dto;
+
+public class FindMovieDetailResponse {
+    private String movieName;
+    private String director;
+    private String description;
+    private String grade;
+    private String review;
+
+    public FindMovieDetailResponse(String movieName, String director, String description) {
+        this.movieName = movieName;
+        this.director = director;
+        this.description = description;
+    }
+}
--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -1,9 +1,20 @@
 package com.example.filmpass.domain.movie.service;
 
+import com.example.filmpass.domain.movie.dto.FindMovieDetailResponse;
+import com.example.filmpass.domain.movie.entity.Movie;
+import com.example.filmpass.domain.movie.repository.MoviceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
+    private final MoviceRepository moviceRepository;
+
+    public void findMovieDtail(Long movieId) {
+        Movie movie = moviceRepository.findById(movieId)
+        FindMovieDetailResponse findMovieDetailResponse = new FindMovieDetailResponse()
+    }
 }

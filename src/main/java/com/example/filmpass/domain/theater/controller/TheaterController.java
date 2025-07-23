5860cc5681fb863fa2da3342d7415b271862b497
권용준 <yoyo91828h@gmail.com>
2025-07-23 16:58:23 +0900

    feat: TheaterController 극장 등록 추가 어도민 권한으로 가능하게 설계

--- src/main/java/com/example/filmpass/domain/theater/controller/TheaterController.java
+++ src/main/java/com/example/filmpass/domain/theater/controller/TheaterController.java
@@ -1,9 +1,28 @@
 package com.example.filmpass.domain.theater.controller;
 
+import com.example.filmpass.domain.theater.dto.TheaterRequest;
+import com.example.filmpass.domain.theater.dto.TheaterResponse;
+import com.example.filmpass.domain.theater.service.TheaterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/theaters")
 public class TheaterController {
+
+    private final TheaterService theaterService;
+
+    // 극장 등록 어도민 권한으로만 가능하게
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<TheaterResponse> createTheater(@RequestBody TheaterRequest request) {
+        TheaterResponse response = theaterService.createTheater(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }

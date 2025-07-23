477133dcd21e602ec0c5e6062ce6e8c2162819e3
Hyeon woo <renzpark@naver.com>
2025-07-23 11:51:27 +0900

    feat: review 목록 조회 API

--- src/main/java/com/example/filmpass/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/filmpass/domain/review/controller/ReviewController.java
@@ -6,6 +6,10 @@ import com.example.filmpass.domain.review.service.ReviewService;
 import com.example.filmpass.global.common.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,4 +38,14 @@ public class ReviewController {
         ReviewResponseDto response = reviewService.updateReview(reviewId, request);
         return ResponseEntity.ok(ApiResponse.success(response, "리뷰가 수정되었습니다."));
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse<Page<ReviewResponseDto>>> getAllReviews(
+            @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
+            Pageable pageable
+    ) {
+        Page<ReviewResponseDto> reviews = reviewService.getAllReviews(pageable);
+        return ResponseEntity.ok(ApiResponse.success(reviews, "리뷰 목록이 조회되었습니다."));
+    }
+
 }
--- src/main/java/com/example/filmpass/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/filmpass/domain/review/repository/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.example.filmpass.domain.review.repository;
 
+import com.example.filmpass.domain.review.dto.ReviewResponseDto;
 import com.example.filmpass.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    Page<Review> findAllByIsDeletedFalse(Pageable pageable);
 }

ecd2f0538052d3db4391f3d0e388d75c6554b873
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-24 16:50:46 +0900

    feat : Page 를 반환하는 쿼리메소드 1개 추가

--- src/main/java/com/example/filmpass/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/filmpass/domain/user/repository/UserRepository.java
@@ -14,5 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
-    Page<User> findAllByDeletedAtIsNullOrderByCreatedAtDesc(Pageable pageable);
+    Page<User> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }

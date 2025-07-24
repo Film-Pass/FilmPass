810b9b491366f98029ce1e14d85fb3d847a88a06
Hyeon woo <renzpark@naver.com>
2025-07-24 16:59:08 +0900

    feat: schedule API 작성

--- src/main/java/com/example/filmpass/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/filmpass/domain/schedule/controller/ScheduleController.java
@@ -1,9 +1,21 @@
 package com.example.filmpass.domain.schedule.controller;
 
+import com.example.filmpass.domain.schedule.dto.ScheduleRequestDto;
+import com.example.filmpass.domain.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/schedules")
 @RequiredArgsConstructor
 public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping
+    public ResponseEntity<String> createSchedule(@RequestBody ScheduleRequestDto requestDto) {
+        scheduleService.createSchedule(requestDto);
+        return ResponseEntity.ok("상영 일정이 등록되었습니다.");
+    }
 }

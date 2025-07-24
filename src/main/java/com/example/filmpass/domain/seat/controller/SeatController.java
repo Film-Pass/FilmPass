390bb763541974b8af8c8f8f8c7bb324f1aeae1f
권용준 <yoyo91828h@gmail.com>
2025-07-24 14:28:51 +0900

    fix: 에러코드 해결

--- src/main/java/com/example/filmpass/domain/seat/controller/SeatController.java
+++ src/main/java/com/example/filmpass/domain/seat/controller/SeatController.java
@@ -1,9 +1,17 @@
 package com.example.filmpass.domain.seat.controller;
 
+import com.example.filmpass.domain.seat.service.SeatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/seats")
 public class SeatController {
+
+    private final SeatService seatService;
+
+
+
 }
--- src/main/java/com/example/filmpass/domain/theater/dto/TheaterRequest.java
+++ src/main/java/com/example/filmpass/domain/theater/dto/TheaterRequest.java
@@ -1,13 +1,11 @@
 package com.example.filmpass.domain.theater.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class TheaterResponse {
-    private Long id;
+public class TheaterRequest {
     private String name;
     private String location;
 }

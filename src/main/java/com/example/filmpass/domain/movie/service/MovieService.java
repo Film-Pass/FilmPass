b47f902627a8d2df4d9cda043a5a3cebff33fb30
Hyeon woo <renzpark@naver.com>
2025-07-24 16:20:20 +0900

    feat: movie 전체 조회

--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -1,7 +1,7 @@
 package com.example.filmpass.domain.movie.service;
 
 import com.example.filmpass.domain.movie.entity.Movie;
-import com.example.filmpass.domain.movie.repository.MoviceRepository;
+import com.example.filmpass.domain.movie.repository.MovieRepository;
 import com.example.filmpass.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +11,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MovieService {
-    private final MoviceRepository moviceRepository;
+    private final MovieRepository movieRepository;
 
     //영화 전체 조회
     public ApiResponse<List<Movie>> findAllMovie() {
-        List<Movie> movieList = moviceRepository.findAll();
+        List<Movie> movieList = movieRepository.findAll();
         if(movieList.isEmpty()) {
             return ApiResponse.success(movieList,"영화가 존재하지 않습니다");
         }

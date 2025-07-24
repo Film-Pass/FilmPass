26ede3c1a39f0ade37b0cd6735c02245256109e0
YoungJae1118 <apricity1118@gmail.com>
2025-07-24 14:54:11 +0900

    feat: 영화 삭제 기능 개발

--- src/main/java/com/example/filmpass/domain/movie/controller/MovieController.java
+++ src/main/java/com/example/filmpass/domain/movie/controller/MovieController.java
@@ -1,9 +1,22 @@
 package com.example.filmpass.domain.movie.controller;
 
+import com.example.filmpass.domain.movie.service.MovieService;
+import com.example.filmpass.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/movies")
 @RequiredArgsConstructor
 public class MovieController {
+    private final MovieService movieService;
+
+    //영화 삭제
+    @DeleteMapping("/{movieId}")
+    public ApiResponse<Object> deleteMovieApi(@PathVariable Long movieId) {
+        return movieService.deleteMovie(movieId);
+    }
 }
--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -1,9 +1,28 @@
 package com.example.filmpass.domain.movie.service;
 
+import com.example.filmpass.domain.movie.entity.Movie;
+import com.example.filmpass.domain.movie.repository.MovieRepository;
+import com.example.filmpass.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
+    private final MovieRepository movieRepository;
+
+    //영화 삭제
+    public ApiResponse<Object> deleteMovie(Long movieId) {
+        Optional<Movie> findMovie = movieRepository.findById(movieId);
+
+        if(findMovie.isEmpty()) {
+            ApiResponse.error("Id에 해당하는 영화가 없습니다");
+        }
+        Movie movie = findMovie.get();
+        movieRepository.deleteById(movieId);
+
+        return ApiResponse.success(movie, "영화가 성공적으로 삭제되었습니다.");
+    }
 }

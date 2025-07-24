5a2cd0fabb86d978ecd64e016dcb73e6ff31af69
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-24 16:48:48 +0900

    feat : ADMIN 권한 체크 예외 추가

--- src/main/java/com/example/filmpass/global/exception/ErrorCode.java
+++ src/main/java/com/example/filmpass/global/exception/ErrorCode.java
@@ -19,7 +19,8 @@ public enum ErrorCode {
 
     // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    DELETED_USER(HttpStatus.BAD_REQUEST, "이미 탈퇴한 유저입니다.");
+    DELETED_USER(HttpStatus.BAD_REQUEST, "이미 탈퇴한 유저입니다."),
+    NOT_ADMIN(HttpStatus.BAD_REQUEST, "관리자 권한이 없습니다.");
 
     private final HttpStatus code;
     private final String message;

d97b2f2090774356a78b332e46698b656f08780a
권용준 <yoyo91828h@gmail.com>
2025-07-25 20:30:06 +0900

    feat: Service 좌석 등록 기능

--- src/main/java/com/example/filmpass/domain/seat/service/SeatService.java
+++ src/main/java/com/example/filmpass/domain/seat/service/SeatService.java
@@ -1,10 +1,15 @@
 package com.example.filmpass.domain.seat.service;
 
 
+import com.example.filmpass.domain.screen.entity.Screen;
+import com.example.filmpass.domain.screen.repository.ScreenRepository;
 import com.example.filmpass.domain.seat.dto.PagedResponse;
+import com.example.filmpass.domain.seat.dto.SeatRequest;
 import com.example.filmpass.domain.seat.dto.SeatResponse;
 import com.example.filmpass.domain.seat.entity.Seat;
 import com.example.filmpass.domain.seat.repository.SeatRepository;
+import com.example.filmpass.global.exception.CustomException;
+import com.example.filmpass.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +23,22 @@ import java.util.stream.Collectors;
 public class SeatService {
 
     private final SeatRepository seatRepository;
+    private final ScreenRepository screenRepository; // 상영관 조회용
+
+    // 좌석 등록
+    public SeatResponse createSeat(SeatRequest request) {
+        // 상영관 존재 확인
+        Screen screen = screenRepository.findById(request.getScreen_Id())
+                .orElseThrow(() ->  new CustomException(ErrorCode.SCREEN_NOT_FOUND));
+
+        // 2. 좌석 엔티티 생성
+        Seat seat = new Seat(screen, request.getSeat_Number());
+
+        // 저장
+        Seat savedSeat = seatRepository.save(seat);
+
+        return new SeatResponse(savedSeat.getId(), savedSeat.getSeat_Number());
+    }
 
     // 좌석 목록 조회 (페이징)
     public PagedResponse<SeatResponse> getSeats(Pageable pageable) {

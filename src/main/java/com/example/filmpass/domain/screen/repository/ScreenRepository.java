a931ee83fcb9c26d331acb5edefcdac6170fb5f5
권용준 <yoyo91828h@gmail.com>
2025-07-25 20:31:28 +0900

    feat: 상영관과의 연관관계로 인해서 추가

---
+++ src/main/java/com/example/filmpass/domain/screen/repository/ScreenRepository.java
@@ -0,0 +1,8 @@
+package com.example.filmpass.domain.screen.repository;
+
+import com.example.filmpass.domain.screen.entity.Screen;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreenRepository extends JpaRepository<Screen, Long> {
+}

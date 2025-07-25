decb2bf68452e6ff8267814b87296aad2f80bf83
YoungJae1118 <apricity1118@gmail.com>
2025-07-25 15:49:05 +0900

    feat/(#34) : 영화 삭제 기능 추가

--- src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
+++ src/main/java/com/example/filmpass/domain/movie/service/MovieService.java
@@ -1,7 +1,7 @@
 package com.example.filmpass.domain.movie.service;
 
 import com.example.filmpass.domain.movie.entity.Movie;
-import com.example.filmpass.domain.movie.repository.MoviceRepository;
+import com.example.filmpass.domain.movie.repository.MovieRepository;
 import com.example.filmpass.global.common.ApiResponse;
 import com.example.filmpass.domain.movie.dto.MovieCreateRequest;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +12,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MovieService {
-    private final MoviceRepository moviceRepository;
+    private final MovieRepository movieRepository;
 
     //영화 등록 CreateMovie
     public ApiResponse<String> movieCreate(MovieCreateRequest movieCreateRequest){
@@ -23,14 +23,14 @@ public class MovieService {
         String title = movieCreateRequest.getMovieName();
 
         Movie movie = new Movie(runningTime,director,description,posterUrl,title);
-        moviceRepository.save(movie);
+        movieRepository.save(movie);
 
         return ApiResponse.success(movie.getTitle(),"영화 등록 성공!");
     }
 
     //영화 전체 조회
     public ApiResponse<List<Movie>> findAllMovie() {
-        List<Movie> movieList = moviceRepository.findAll();
+        List<Movie> movieList = movieRepository.findAll();
         if(movieList.isEmpty()) {
             return ApiResponse.success(movieList,"영화가 존재하지 않습니다");
         }

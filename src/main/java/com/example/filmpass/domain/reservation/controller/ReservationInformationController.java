ace23cec914b408d8a5f2c1706c278f73eae3a71
imtaehun <luxlaetitia9542@gmail.com>
2025-07-27 15:39:57 +0900

    feat: ReservationController에서 사용자 인증 정보 추출 및 서비스 전달

--- src/main/java/com/example/filmpass/domain/reservation/controller/ReservationInformationController.java
+++
@@ -1,9 +0,0 @@
-package com.example.filmpass.domain.reservation.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-public class ReservationInformationController {
-}

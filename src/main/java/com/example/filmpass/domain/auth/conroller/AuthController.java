5e5ca97c370c3a67e769499244d8495a250bb3a2
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-28 19:36:01 +0900

    fix : 공통응답 형식 변경, 책임분리 docs : 주석추가

--- src/main/java/com/example/filmpass/domain/auth/conroller/AuthController.java
+++ src/main/java/com/example/filmpass/domain/auth/conroller/AuthController.java
@@ -1,7 +1,7 @@
 package com.example.filmpass.domain.auth.conroller;
 
 import com.example.filmpass.domain.auth.dto.LoginRequestDto;
-import com.example.filmpass.domain.auth.dto.RoleReuqestDto;
+import com.example.filmpass.domain.auth.dto.RoleRequestDto;
 import com.example.filmpass.domain.auth.dto.SignUpRequestDto;
 import com.example.filmpass.domain.auth.service.AuthService;
 import com.example.filmpass.global.common.ApiResponse;
@@ -9,7 +9,6 @@ import com.example.filmpass.global.config.UserPrincipal;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,45 +18,49 @@ public class AuthController {
 
     private final AuthService authService;
 
+    // 회원가입
     @PostMapping("/api/auth/signup")
-    public ResponseEntity<ApiResponse<?>> signUp(
+    public ApiResponse signUp(
             @Valid @RequestBody SignUpRequestDto requestDto
     ) {
 
-        return ResponseEntity.ok(authService.signUp(requestDto));
+        return ApiResponse.success(authService.signUp(requestDto), "회원가입 성공!");
 
     }
 
+    // 로그인
     @PostMapping("/api/auth/login")
-    public ResponseEntity<ApiResponse<?>> login(
+    public ApiResponse login(
             @RequestBody LoginRequestDto requestDto,
             HttpServletResponse response
             ) {
 
-        return ResponseEntity.ok(authService.login(requestDto, response));
+        return ApiResponse.success(authService.login(requestDto, response), "로그인 성공!");
 
     }
 
+    // 로그아웃
     @PostMapping("/api/auth/logout")
-    public ResponseEntity<ApiResponse<?>> logout(
+    public ApiResponse logout(
             @AuthenticationPrincipal UserPrincipal principal,
             HttpServletResponse response
     ) {
 
         Long userId = principal.getUserId();
 
-        return ResponseEntity.ok(authService.logout(userId, response));
+        return ApiResponse.success(null, "로그아웃 성공!");
 
     }
 
+    // 권한 변경
     @PatchMapping("/api/auth/{id}")
-    public ResponseEntity<ApiResponse<?>> changeRole(
-            @RequestBody RoleReuqestDto request,
+    public ApiResponse changeRole(
+            @RequestBody RoleRequestDto request,
             @PathVariable Long id,
             @AuthenticationPrincipal UserPrincipal principal
             ) {
 
-        return ResponseEntity.ok(authService.changeRole(request, id, principal));
+        return ApiResponse.success(authService.changeRole(request, id, principal), "권한 변경 성공!");
 
     }
 

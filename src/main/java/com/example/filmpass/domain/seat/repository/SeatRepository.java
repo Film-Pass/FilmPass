e2d9557f2416bb98872238d9746f74f2c84e08fb
권용준 <yoyo91828h@gmail.com>
2025-08-01 21:06:22 +0900

    feat: SeatRepository ->  existsByScreenIdAndSeatNumber 중복 체크를 위한 추가

--- src/main/java/com/example/filmpass/domain/seat/repository/SeatRepository.java
+++ src/main/java/com/example/filmpass/domain/seat/repository/SeatRepository.java
@@ -4,4 +4,5 @@ import com.example.filmpass.domain.seat.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+    boolean existsByScreenIdAndSeatNumber(Long id, String seatNumber);
 }

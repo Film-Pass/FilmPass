501006b45a704f45f9f27f14a6e234adaafd4704
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-08-01 19:43:30 +0900

    fix : NOT BLANK -> NOT NULL 로 변경

--- src/main/java/com/example/filmpass/domain/auth/dto/RoleRequestDto.java
+++ src/main/java/com/example/filmpass/domain/auth/dto/RoleRequestDto.java
@@ -2,12 +2,13 @@ package com.example.filmpass.domain.auth.dto;
 
 import com.example.filmpass.domain.user.enums.UserRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class RoleRequestDto {
 
-    @NotBlank(message = "권한을 입력해주세요.")
+    @NotNull(message = "권한을 입력해주세요.")
     private UserRole userRole;
 
 }

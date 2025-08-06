42eb5840d836141c46940f159707813ef4580f2e
Hyeon woo <renzpark@naver.com>
2025-08-06 12:33:34 +0900

    fix review repository isdeleted 문삭제

--- src/main/java/com/example/filmpass/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/filmpass/domain/review/repository/ReviewRepository.java
@@ -14,7 +14,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Optional<Review> findById(Long reviewId);
     Page<Review> findAllByMovie(Movie movie, Pageable pageable);
-    Optional<Review> findByReviewIdAndIsDeletedFalse(Long reviewId);
-    Page<Review> findAllByMovieAndIsDeletedFalse(Movie movie, Pageable pageable);
+
     List<Review> findAllByMovieId(Long movieId);
 }

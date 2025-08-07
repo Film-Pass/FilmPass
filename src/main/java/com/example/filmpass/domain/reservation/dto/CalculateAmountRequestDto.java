9c267079d9dbc8ce433d941742a52f58c339cd82
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-08-07 13:20:57 +0900

    feat : 테스트용 생성자 생성

--- src/main/java/com/example/filmpass/domain/reservation/dto/CalculateAmountRequestDto.java
+++ src/main/java/com/example/filmpass/domain/reservation/dto/CalculateAmountRequestDto.java
@@ -1,11 +1,15 @@
 package com.example.filmpass.domain.reservation.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CalculateAmountRequestDto {
 
     @NotEmpty(message = "하나 이상의 좌석을 선택해주세요.")

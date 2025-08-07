a85a93845f5d2769b06a98ed72f06c18d494399c
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-08-07 10:55:37 +0900

    feat : 테스트용 생성자 생성

--- src/main/java/com/example/filmpass/domain/auth/dto/DiscountTypeRequestDto.java
+++ src/main/java/com/example/filmpass/domain/auth/dto/DiscountTypeRequestDto.java
@@ -2,9 +2,13 @@ package com.example.filmpass.domain.auth.dto;
 
 import com.example.filmpass.domain.user.enums.DiscountType;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DiscountTypeRequestDto {
 
     @NotNull(message = "할인 타입을 입력해주세요.")

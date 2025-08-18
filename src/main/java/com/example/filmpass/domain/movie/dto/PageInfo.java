25664822a6bc08deb7f03360c8d2a5098fe48d37
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-08-18 09:17:00 +0900

    feat : 기본 생성자 추가

--- src/main/java/com/example/filmpass/domain/movie/dto/PageInfo.java
+++ src/main/java/com/example/filmpass/domain/movie/dto/PageInfo.java
@@ -2,8 +2,10 @@ package com.example.filmpass.domain.movie.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PageInfo {
     private int currentPage;
     private int totalPages;

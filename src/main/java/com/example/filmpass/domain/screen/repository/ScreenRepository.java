2a582ec8b1d0d6db3dd85e7d99828a15b0381be6
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-08-21 21:55:26 +0900

    feat : 쿼리메서드 1종 추가

--- src/main/java/com/example/filmpass/domain/screen/repository/ScreenRepository.java
+++ src/main/java/com/example/filmpass/domain/screen/repository/ScreenRepository.java
@@ -1,12 +1,17 @@
 package com.example.filmpass.domain.screen.repository;
 
 import com.example.filmpass.domain.screen.entity.Screen;
+import com.example.filmpass.domain.theater.entity.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ScreenRepository extends JpaRepository<Screen, Long> {
 
     boolean existsByNameAndTheaterId(String name, Long theaterId);
 
     boolean existsByNameAndTheaterIdAndIdNot(String name, Long theaterId, Long id);
+
+    List<Screen> findByTheater(Theater theater);
 
 }

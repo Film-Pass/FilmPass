36d0da515080c8b64e5186e4be3d4656748520a7
Gwon-YJ <yoyo91828h@gmail.com>
2025-08-25 17:40:07 +0900

    도전

--- src/main/java/com/example/filmpass/domain/seat/dto/SeatSimpleResponse.java
+++ src/main/java/com/example/filmpass/domain/seat/dto/SeatSimpleResponse.java
@@ -1,17 +1,12 @@
 package com.example.filmpass.domain.seat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class SeatSimpleResponse {
     private Long id;
     private String seatNumber;
     private SeatStatus status;
-
-    public SeatSimpleResponse(Long id, String seatNumber, SeatStatus status) {
-        this.id = id;
-        this.seatNumber = seatNumber;
-        this.status = status;
-    }
-
-    public Long getId() { return id; }
-    public String getSeatNumber() { return seatNumber; }
-    public SeatStatus getStatus() { return status; }
 }
